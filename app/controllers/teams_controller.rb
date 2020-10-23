fbbb81d9853134c40782a0022adb9da04b728636
Cailie.Skelton <CSkelton707@gmail.com>
2020-10-23 00:15:41 -0400

    started building out teams controller and index erb

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,11 +1,29 @@
- #CREATE a tournament-
-        #-shows an index page of all the tournaments
+ class TeamsController < ApplicationController
+ #CREATE a team-
+     #New  
+        #make a get request to '/teams/new'
+    #Create
+        #make a post request to '/teams'
 
+    #READ
+    get '/teams' do
+        @teams = Teams.all
+       erb :'teams/index'
+    end       
+    
+    #Index 
+        #make a get request to '/teams'
 
-    #READ a tournament
-        #Index all the tournaments  #make a get request to /tournament
-
-        #Show a single tournament
+        #Show 
+        #make a get request to '/teams/:id'
+    
     #UPDATE a tournament
+        #edit
+            #make a get request to '/teams/:id/edit'
+        #update
+            #make a patch request to '/teams/:id'
 
     #DELETE a tournament
+        #make a delete request to ,/recepies/:id'
+
+ end
